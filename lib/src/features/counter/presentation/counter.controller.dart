72d2179637c4bed2d94ea1c33362e096a68adca6
Oualid Boutakhrit <Oualid.Boutakhrit@coodoo.de>
2022-06-09 14:23:57 +0200

    solves provider classes with riverpod

--- lib/src/features/counter/presentation/counter.controller.dart
+++ lib/src/features/counter/presentation/counter.controller.dart
@@ -1,25 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_architektur_workshop/src/features/counter/data/counter.repo.dart';
 import 'package:flutter_architektur_workshop/src/features/counter/domain/counter.entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterControllerProvider = StateNotifierProvider<CounterController, void>(
-  (ref) {
-    return CounterController(
-      ref.read,
-      Counter(counter: ref.watch(counterRepoProvider).counter),
-    );
-  },
-);
+final counterControllerProvider = ChangeNotifierProvider((ref) {
+  return CounterController(
+    ref.read,
+    Counter(
+      counter: ref.watch(counterRepoProvider).counter,
+    ),
+  );
+});
 
-class CounterController extends StateNotifier {
-  CounterController(this.read, this._counterEnitity) : super(null);
+class CounterController extends ChangeNotifier {
+  CounterController(this.read, this._counter);
 
-  final Reader read;
-  final Counter _counterEnitity;
+  Reader read;
+  Counter _counter;
 
-  int get counter => _counterEnitity.counter;
+  int get counter => _counter.counter;
 
   increment() {
     read(counterRepoProvider).increment();
+    notifyListeners();
   }
 }
--- lib/src/features/dashboard/presentation/dashboard.controller.dart
+++ lib/src/features/dashboard/presentation/dashboard.controller.dart
@@ -1,16 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_architektur_workshop/src/features/counter/data/counter.repo.dart';
 import 'package:flutter_architektur_workshop/src/features/counter/domain/counter.entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dashboardControllerProvider = StateNotifierProvider<DashboardController, void>(
-  (ref) => DashboardController(
-    Counter(counter: ref.watch(counterRepoProvider).counter),
-  ),
-);
+final dashboardControllerProvider = ChangeNotifierProvider((ref) {
+  return DashboardController(Counter(counter: ref.watch(counterRepoProvider).counter));
+});
 
-class DashboardController extends StateNotifier {
-  DashboardController(this._counterEntity) : super(null);
-  final Counter _counterEntity;
+class DashboardController extends ChangeNotifier {
 
-  int get counter => _counterEntity.counter;
+  late Counter _counter;
+  DashboardController(this._counter);
+
+  int get counter => _counter.counter;
 }
+
